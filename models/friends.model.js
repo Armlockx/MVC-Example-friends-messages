627a6b146b1d699678f1579b43f44e0fc2a82d26
Armlockx <reus_julio@hotmail.com>
2024-01-29 12:33:55 -0300

    mvc implementation

---
+++ models/friends.model.js
@@ -0,0 +1,16 @@
+const friends = [
+    {
+        id: 0,
+        name: 'Albert Einstein',
+    },
+    {
+        id: 1,
+        name: 'Nikola Tesla',
+    },
+    {
+        id: 2,
+        name: 'Jackiie Chan',
+    }
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const friendsController = require('./controllers/friends.controller');
+const messagesController = require('./controllers/messages.controller');
+
+const app = express();
+
+const PORT = 3000;
+
+//MUST BE THE FIRST ROUT HANDLER!!!!!!!! 'logging'
+app.use((req, res, next) => {
+    const start = Date.now();
+    next(); //goes to the next rout, going in
+    const delta = Date.now() - start;   //going out
+    console.log(`${req.method} ${req.url} ${delta} ms`);
+});
+
+app.use(express.json());    //req.body : content-type = application/json (when matching)
+
+app.post('/friends', friendsController.postFriend);
+
+app.get('/friends', friendsController.getFriends);
+
+app.get('/friends/:friendId', friendsController.postFriend);
+
+app.get('/messages', messagesController.getMessages);
+
+app.post('/messages', messagesController.postMessage);
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`);
+});
